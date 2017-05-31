c9fe8dfb604ce98227d104988b224cb93cad1a68
oscarnw <onwingrove@gmail.com>
2017-05-31 23:42:35 +0100

    added buyFarm function

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 
 
 var dollar = 0
+var farm = 0
 
 function getDollar(number){
            dollar = dollar + number;
            document.getElementById("dollar").innerHTML = dollar;
        };
 
+function BuyFarm(){
+           farm = farm + 1;
+           document.getElementById("farm").innerHTML = farm;
+       };
