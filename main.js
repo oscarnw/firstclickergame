fb6039749d93cae806f0aef880423eb3572bbb3e
oscarnw <onwingrove@gmail.com>
2017-05-31 23:54:51 +0100

    finished buyfarm func with cost

--- main.js
+++ main.js
@@ -2,13 +2,23 @@
 
 var dollar = 0
 var farm = 0
+var farmCost = 1000
+
+
+
+
+
 
 function getDollar(number){
            dollar = dollar + number;
            document.getElementById("dollar").innerHTML = dollar;
        };
 
-function buyFarm(number){
-           farm = farm + number;
-           document.getElementById("farm").innerHTML = farm;
+function buyFarm(){
+           if(dollar >= farmCost){
+                      farms = farms + 1;
+                      dollar = dollar - farmCost
+                      document.getElementById("dollar").innerHTML = dollar;
+                      document.getElementById("farm").innerHTML = farm;
+           };
        };
