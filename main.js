815474e63a01fcc890b06567e5b281f372fba77f
oscarnw <onwingrove@gmail.com>
2017-05-31 20:06:55 +0100

    farms always cost 1000

--- main.js
+++ main.js
@@ -8,13 +8,10 @@ function GetDollar(number){
 };
 
 function buyFarm(){
-    var farmCost = Math.floor(10 * Math.pow(1.1,farms));     //works out the cost of this cursor
-    if(dollars >= farmCost){                                   //checks that the player can afford the cursor
-        farms = farms + 1;                                   //increases number of cursors
-    	dollars = dollars - farmCost;                          //removes the cookies spent
-        document.getElementById('farms').innerHTML = farms;  //updates the number of cursors for the user
-        document.getElementById('dollars').innerHTML = dollars;  //updates the number of cookies for the user
-    };
-    var nextCost = Math.floor(10 * Math.pow(1.1,farms));       //works out the cost of the next cursor
-    document.getElementById('farmCost').innerHTML = nextCost;  //updates the cursor cost for the user
+    var farmCost = 1000;
+    if(dollars >= farmCost){                               
+        farms = farms + 1;                                 
+    	dollars = dollars - farmCost;                         
+        document.getElementById('farms').innerHTML = farms;
+        document.getElementById('dollars').innerHTML = dollars;
 };
