e378f9cb5a325965d2e8b4ecbf4ac46da50c67ff
oscarnw <onwingrove@gmail.com>
2017-05-31 20:00:04 +0100

    changing getDollar to dollars

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var farms = 0;
 
 function GetDollar(number){
     dollars = dollars + number;
-    document.getElementById("GetDollar").innerHTML = dollars;
+    document.getElementById("dollars").innerHTML = dollars;
 };
 
 function buyFarm(){
@@ -13,7 +13,7 @@ function buyFarm(){
         farms = farms + 1;                                   //increases number of cursors
     	dollars = dollars - farmCost;                          //removes the cookies spent
         document.getElementById('farms').innerHTML = farms;  //updates the number of cursors for the user
-        document.getElementById('GetDollar').innerHTML = dollars;  //updates the number of cookies for the user
+        document.getElementById('dollars').innerHTML = dollars;  //updates the number of cookies for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,farms));       //works out the cost of the next cursor
     document.getElementById('farmCost').innerHTML = nextCost;  //updates the cursor cost for the user
