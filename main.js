23913752d6f143d7a92ac11249e9f949d13118c5
oscarnw <onwingrove@gmail.com>
2017-05-31 20:44:54 +0100

    removed some html

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ var farms = 0;
 var farmCost = 1000;
 var test = 0
 
-    <p> <button onclick="Test(1)">Get 1 Test Currency</button> </p>
-	   <p> <button onclick="Test(2)">Get 2 Test Currencies</button> </p>
+  
 
 function Test(number){
            test = test + number;
