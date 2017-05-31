bebb2aa1ddcab43ab93fb4f94de4b7a3fb8ddc38
oscarnw <onwingrove@gmail.com>
2017-05-31 20:46:35 +0100

    simplified

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
 
-var dollars = 0;
-var farms = 0;
-var farmCost = 1000;
-var test = 0
 
-  
+var test = 0
 
 function Test(number){
            test = test + number;
            document.getElementById("test").innerHTML = test;
        };
-
-function getDollar(number){
-    dollars = dollars + number;
-    document.getElementById("dollars").innerHTML = dollars;
-};
-
-function buyFarm(){
-        if(dollars >= farmCost){                               
-        farms = farms + 1;                                 
-    	dollars = dollars - farmCost;                         
-        document.getElementById('farms').innerHTML = farms;
-        document.getElementById('dollars').innerHTML = dollars;
-};
 
