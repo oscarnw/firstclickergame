916c2c35c7109c568c501fa27480396ec0b3b738
oscarnw <onwingrove@gmail.com>
2017-05-31 20:42:56 +0100

    added test currency
    function Test(number){
               test = test + number;
               document.getElementById("test").innerHTML = test;
           };

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 var dollars = 0;
 var farms = 0;
 var farmCost = 1000;
+var test = 0
+
+    <p> <button onclick="Test(1)">Get 1 Test Currency</button> </p>
+	   <p> <button onclick="Test(2)">Get 2 Test Currencies</button> </p>
+
+function Test(number){
+           test = test + number;
+           document.getElementById("test").innerHTML = test;
+       };
 
 function getDollar(number){
     dollars = dollars + number;
