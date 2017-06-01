c43f11c2cf4df75f5d2daa26b93fd8826263a6b2
oscarnw <onwingrove@gmail.com>
2017-06-01 03:16:53 +0100

    update 3 from cloud9 adding detectors

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
-
 var dollar = 0
 var farm = 0
 var farmCost = 0
 var tractor = 0
 var tractorCost = 0
+var detector = 0
+var detectorCost = 0
 var earnpersec = 0
+var gold = 0
 
 
 function getDollar(number){
@@ -22,7 +24,7 @@ function buyFarm(number){
                       farm = farm + number;
                       dollar = dollar - farmCost
                                var farmCost =  Math.floor(1000 * Math.pow(1.1,farm));             
-                      document.getElementById("dollar").innerHTML = dollar;
+                      document.getElementById("dollar").innerHTML = Math.round(dollar);
                       document.getElementById("farm").innerHTML = farm;
                       document.getElementById("farmCost").innerHTML = farmCost;
            };
@@ -41,18 +43,45 @@ function buyTractor(number){
                                var tractorCost =  Math.floor(1000 * Math.pow(1.1,tractor));             
                       document.getElementById("dollar").innerHTML = dollar;
                       document.getElementById("tractor").innerHTML = tractor;
-                      document.getElementById("farmCost").innerHTML = tractorCost;
+                      document.getElementById("tractorCost").innerHTML = tractorCost;
            };
-           var earnpersec = earnpersec = 50 * farm * Math.pow(1.01,tractor)
+          var earnpersec = earnpersec = 50 * farm * Math.pow(1.01,tractor)
            document.getElementById("earnpersec").innerHTML = Math.round(earnpersec);
        };
-
-
-
+        
+   
+   function buyDetector(number){
+        
+    var detectorCost =  Math.floor(1000 * Math.pow(1.1,detector));
+    
+    if(detector < 5){
+           if(dollar >= detectorCost){
+                      detector = detector + number;
+                      dollar = dollar - detectorCost
+                               var detectorCost =  Math.floor(1000 * Math.pow(1.1,detector));             
+                      document.getElementById("dollar").innerHTML = dollar;
+                      document.getElementById("detector").innerHTML = detector;
+                      document.getElementById("detectorCost").innerHTML = detectorCost;
+           };
+    };
+          var earnpersec = earnpersec = 50 * farm * Math.pow(1.01,tractor)
+           document.getElementById("earnpersec").innerHTML = Math.round(earnpersec);
+       };
+   
+   
+   function sellGold(number){
+       if(gold >= 1){
+	dollar = dollar + 1000*number;
+	gold = gold - number
+	document.getElementById("dollar").innerHTML = dollar;
+	document.getElementById("gold").innerHTML = gold;
+       }
+   }
+   
+   
 window.setInterval(function(){
 	
 	getDollar(50 * farm * Math.pow(1.01,tractor) );
 
-	
 	
 }, 1000);
