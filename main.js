80ea5890215abbfde60cda1c8068fafb2bc34105
oscarnw <onwingrove@gmail.com>
2017-06-01 02:38:23 +0100

    update 2 from cloud 9 adding tractors

--- main.js
+++ main.js
@@ -2,12 +2,15 @@
 
 var dollar = 0
 var farm = 0
-
+var farmCost = 0
+var tractor = 0
+var tractorCost = 0
+var earnpersec = 0
 
 
 function getDollar(number){
            dollar = dollar + number;
-           document.getElementById("dollar").innerHTML = dollar;
+           document.getElementById("dollar").innerHTML = Math.round(dollar);
        };
 
 function buyFarm(number){
@@ -23,11 +26,33 @@ function buyFarm(number){
                       document.getElementById("farm").innerHTML = farm;
                       document.getElementById("farmCost").innerHTML = farmCost;
            };
+          var earnpersec = earnpersec = 50 * farm * Math.pow(1.01,tractor)
+           document.getElementById("earnpersec").innerHTML = Math.round(earnpersec);
        };
+
+function buyTractor(number){
+        
+    var tractorCost =  Math.floor(1000 * Math.pow(1.1,tractor));
+    
+    
+           if(dollar >= tractorCost){
+                      tractor = tractor + number;
+                      dollar = dollar - tractorCost
+                               var tractorCost =  Math.floor(1000 * Math.pow(1.1,tractor));             
+                      document.getElementById("dollar").innerHTML = dollar;
+                      document.getElementById("tractor").innerHTML = tractor;
+                      document.getElementById("farmCost").innerHTML = tractorCost;
+           };
+           var earnpersec = earnpersec = 50 * farm * Math.pow(1.01,tractor)
+           document.getElementById("earnpersec").innerHTML = Math.round(earnpersec);
+       };
+
 
 
 window.setInterval(function(){
 	
-	getDollar(50 * farm);
+	getDollar(50 * farm * Math.pow(1.01,tractor) );
+
+	
 	
 }, 1000);
