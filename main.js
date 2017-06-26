7fc36ce959193688f6ebb719d07143a0671438dc
Oscarnw <onwingrove@gmail.com>
2017-06-26 01:36:14 +0100

    get seed buttons

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 var money = 0;
+var appleSeed = 0;
+var mangoSeed = 0;
+var poisonRootSeed = 0;
+var magicBeanSeed = 0;
 
 function getCoin(){
     money ++
-		    document.getElementById("goblinBankAccount").innerHTML = money;
+    document.getElementById("goblinBankAccount").innerHTML = money;
 };
+
+function getAppleSeed(){
+    appleSeed ++
+    document.getElementById("appleSeed").innerHTML = appleSeed;
+}
+
+function getMangoSeed(){
+    mangoSeed ++
+    document.getElementById("mangoSeed").innerHTML = mangoSeed;
+}
+
+function getPoisonRootSeed(){
+    poisonRootSeed ++
+    document.getElementById("poisonRootSeed").innerHTML = poisonRootSeed;
+}
+
+
