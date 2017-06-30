d2377b6e89435c37c17fb0cd5600805658bd6801
Oscarnw <onwingrove@gmail.com>
2017-06-30 17:27:15 +0100

    added sounds

--- main.js
+++ main.js
@@ -1,27 +1,46 @@
 var money = 0;
-var appleSeed = 0;
-var mangoSeed = 0;
-var poisonRootSeed = 0;
-var magicBeanSeed = 0;
+
+var appleSeed = 0; var mangoSeed = 0; var poisonRootSeed = 0; var magicBeanSeed = 0;
+
+var mangoSeedUnlocked = false; var poisonRootSeedUnlocked = false; var magicBeanSeedUnlocked = false;
+
+var soundGetCoin = document.getElementById("soundGetCoin");
+var soundPickingSeed = document.getElementByID("soundPickingSeed");
 
 function getCoin(){
     money ++
     document.getElementById("goblinBankAccount").innerHTML = money;
-};
+    soundGetCoin.play()
+}
 
 function getAppleSeed(){
     appleSeed ++
     document.getElementById("appleSeed").innerHTML = appleSeed;
+    soundGetCoin.play()
 }
 
-function getMangoSeed(){
-    mangoSeed ++
-    document.getElementById("mangoSeed").innerHTML = mangoSeed;
+function getMangoSeed() {
+    if (mangoSeedUnlocked == true) {
+        mangoSeed++
+        document.getElementById("mangoSeed").innerHTML = mangoSeed;;
+        soundPickingSeed.play()
+    }
 }
 
-function getPoisonRootSeed(){
-    poisonRootSeed ++
-    document.getElementById("poisonRootSeed").innerHTML = poisonRootSeed;
+function getPoisonRootSeed() {
+    if (poisonRootSeedUnlocked == true) {
+        poisonRootSeed++
+        document.getElementById("poisonRootSeed").innerHTML = poisonRootSeed;
+        soundPickingSeed.play()
+    }
+}
+
+function getMagicBeanSeed() {
+    if (magicBeanSeedUnlocked == true) {
+        magicBeanSeed++
+        document.getElementById("magicBeanSeed").innerHTML = magicBeanSeed;
+        soundPickingSeed.play()
+    }
 }
 
 
